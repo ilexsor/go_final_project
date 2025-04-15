b23978a40fd04869da66c2ce072009a1cd550777
ilexsor <ilexsor@gmail.com>
2025-04-15 08:10:14 +0400

    add AddTack handler

--- backend/internal/handlers/handlers.go
+++ backend/internal/handlers/handlers.go
@@ -1,12 +1,18 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/ilexsor/internal/models"
 	"github.com/ilexsor/internal/utils"
 	log "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 )
 
 // FileServer Handler для обработки статических файлов
@@ -79,9 +85,51 @@ func NextDayHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // Обработчик для /api/task
-func AddTask(w http.ResponseWriter, r *http.Request) {
+func AddTask(db *gorm.DB) http.HandlerFunc {
 
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("added"))
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		log.WithFields(log.Fields{
+			"addTask": "incoming request",
+		}).Infof("request:%v", r.URL.String())
+
+		task := models.Scheduler{}
+		body, err := io.ReadAll(r.Body)
+		log.WithFields(log.Fields{
+			"body": body,
+		}).Error()
+
+		if err != nil {
+			log.WithFields(log.Fields{
+				"readBody": "error",
+			}).Errorf("request:%v", r.URL.String())
+		}
+
+		err = json.Unmarshal(body, &task)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"unmarshal": "error",
+			}).Errorf("error:%v", err)
+			http.Error(w, "Ошибка анмаршаллинга", http.StatusBadRequest)
+			return
+		}
+
+		tx := db.WithContext(ctx).Create(&task)
+		if tx.Error != nil {
+			http.Error(w, "Ошибка при сохранении задачи", http.StatusInternalServerError)
+			return
+		}
+
+		response, err := json.Marshal(fmt.Sprintf("id:\"%v\"", task.ID))
+		if err != nil {
+			http.Error(w, "Ошибка анмаршаллинга", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
+	}
 }
--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -13,3 +13,10 @@ type Scheduler struct {
 func (Scheduler) TableName() string {
 	return "scheduler"
 }
+
+type Task struct {
+	Date    string `json:"date"`
+	Title   string `json:"title"`
+	Comment string `json:"comment"`
+	Repeat  string `json:"repeat"`
+}
